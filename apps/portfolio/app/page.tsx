40ccfa330ea20e34b52a6d30d6c699e65f7611ee
binhnd <tthandb@hotmail.com>
2024-06-22 22:09:01 +0700

    init repo

---
+++ apps/portfolio/app/page.tsx
@@ -0,0 +1,11 @@
+import { Card } from "@repo/ui/card";
+
+export default function Page(): JSX.Element {
+  return (
+    <main className="flex flex-col items-center justify-between min-h-screen p-24">
+      <div className="grid mb-32 text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+        I'm Ryan Nguyen a.k.a Binh Nguyen Duc
+      </div>
+    </main>
+  );
+}
