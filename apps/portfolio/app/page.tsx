4acfeb5e766925111b6f7fb62f6458022ed582d9
binhnd <tthandb@hotmail.com>
2024-06-23 15:52:28 +0700

    homepage

--- apps/portfolio/app/page.tsx
+++ apps/portfolio/app/page.tsx
@@ -1,20 +1,68 @@
-import { GitPullRequest } from "lucide-react";
-import Header from "../components/header";
-import { Button } from "@repo/ui"
+import Image from 'next/image'
+import Header from '../components/header';
+import ContactLink from '../components/contact-link';
 
-export default function Page(): JSX.Element {
+export default function Page(): NextPage {
   return (
     <>
-      <Header />
-      <main>
-        <div>
-          I'm Ryan Nguyen a.k.a Binh Nguyen Duc
+    <Header />
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <section
+        id="home"
+        className="lg:h-screen w-full flex flex-col lg:flex-row items-center px-10  max-w-7xl mx-auto py-28"
+      >
+        <div className="text w-full lg:w-1/2">
+          <h1 className="text-2xl lg:text-3xl">
+            Hi I'm <br />
+            <span className="text-6xl md:text-7xl lg:text-8xl font-semibold bg-clip-text text-transparent bg-gradient-to-br from-primary  to-accent"> Ryan </span>
+          </h1>
+          <p className="lg:text-lg text-muted mt-4 font-medium max-w-xl">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras molestie sed leo blandit lobortis. Fusce eget eleifend nulla. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Morbi gravida non arcu nec tempus. Nullam ornare faucibus auctor. Nam sagittis eu nibh a ultrices. Cras suscipit volutpat tellus id feugiat. Sed accumsan laoreet risus, a sodales augue laoreet non. Integer euismod odio id urna tristique, non euismod nibh finibus. Cras suscipit mattis augue, porta ullamcorper elit ornare nec. Aliquam est sapien, porta ac eleifend at, tristique nec ligula. Fusce eu nulla leo. Pellentesque laoreet ipsum a lectus sagittis, id hendrerit nisl cursus. Pellentesque et vehicula justo. Nunc laoreet finibus magna, venenatis tempor lectus rutrum eget.
+          </p>
+          <ContactLink />
+          <div className="button gap-2 flex items-center flex-col md:flex-row my-5">
+            <a
+              className="inline-flex items-center justify-center whitespace-nowrap ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-11 rounded-md px-8 font-bold text-xl md:w-auto w-full"
+              href="#contact"
+            >
+              Contact Me
+            </a>
+            <button
+              className="inline-flex items-center justify-center whitespace-nowrap ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-11 rounded-md px-8 font-bold text-xl md:w-auto w-full">
+              My Projects
+            </button>
+          </div>
         </div>
-        <Button className="w-full">
-          <GitPullRequest className="mr-2 h-4 w-4" /> GitPullRequest
-        </Button>
-      </main>
-    </>
+        <div className="image w-full lg:w-1/2 relative aspect-square -mt-10 ">
+          <Image
+           src={}
+           {/*  src="https://static.wikia.nocookie.net/valorant/images/c/c5/Sova_Artwork_Full.png/revision/latest"*/}
+          />
+          {/*<img*/}
+          {/*  alt=""*/}
+          {/*  fetchpriority="high"*/}
+          {/*  decoding="async"*/}
+          {/*  data-nimg="fill"*/}
+          {/*  className="w-full aspect-square"*/}
+          {/*  style={{*/}
+          {/*    position: "absolute",*/}
+          {/*    height: "100%",*/}
+          {/*    width: "100%",*/}
+          {/*    left: 0,*/}
+          {/*    top: 0,*/}
+          {/*    right: 0,*/}
+          {/*    bottom: 0,*/}
+          {/*    color: "transparent"*/}
+          {/*  }}*/}
+          {/*  src="https://static.wikia.nocookie.net/valorant/images/c/c5/Sova_Artwork_Full.png/revision/latest"*/}
+          {/*  sizes="100vw"*/}
+          {/*/>*/}
+          <div
+            className="gradient absolute w-full h-1/2 bg-gradient-to-t from-background to-transparent bottom-0 left-0 right-0" />
+        </div>
+      </section>
 
+    </main>
+    </>
   );
 }
--- apps/portfolio/components/header.tsx
+++ apps/portfolio/components/header.tsx
@@ -13,21 +13,9 @@ const Header = () => {
                     <nav className="text-muted gap-8 hidden items-center  lg:flex">
                         <a
                             className="hover:text-foreground hover:scale-110 transition-all"
-                            href="#services"
-                        >
-                            Services
-                        </a>
-                        <a
-                            className="hover:text-foreground hover:scale-110 transition-all"
                             href="#skills"
                         >
                             Skills
-                        </a>
-                        <a
-                            className="hover:text-foreground hover:scale-110 transition-all"
-                            href="#testimonials"
-                        >
-                            Testimonials
                         </a>
                         <a
                             className="hover:text-foreground hover:scale-110 transition-all"
@@ -36,45 +24,6 @@ const Header = () => {
                             Contact
                         </a>
                     </nav>
-                    <button className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-10 px-4 py-2 lg:hidden">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width={24}
-                            height={24}
-                            viewBox="0 0 24 24"
-                            fill="none"
-                            stroke="currentColor"
-                            strokeWidth={2}
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            className="lucide lucide-menu "
-                        >
-                            <line x1={4} x2={20} y1={12} y2={12} />
-                            <line x1={4} x2={20} y1={6} y2={6} />
-                            <line x1={4} x2={20} y1={18} y2={18} />
-                        </svg>
-                        <div className="sr-only">Menu Button</div>
-                    </button>
-                </div>
-                <div
-                    className="mobile-nav absolute lg:hidden top-[120%] mx-auto w-[95vw] left-0 right-0 transition-all overflow-hidden"
-                >
-                    <div className="rounded-lg border bg-card text-card-foreground shadow-sm p-4">
-                        <nav className="flex flex-col gap-4">
-                            <a className="text-lg" href="#services">
-                                Services
-                            </a>
-                            <a className="text-lg" href="#skills">
-                                Skills
-                            </a>
-                            <a className="text-lg" href="#testimonials">
-                                Testimonials
-                            </a>
-                            <a className="text-lg" href="#contact">
-                                Contact
-                            </a>
-                        </nav>
-                    </div>
                 </div>
             </div>
         </header>
